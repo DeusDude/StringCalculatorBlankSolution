d4c87025c9f45ddb7aae6b3ec51e9dde789467ae
DeusDude <matthew_deus@Progressive.com>
2021-11-19 15:58:46 -0500

    Final Day

--- StringCalculatorBlank/StringCalculator.cs
+++ StringCalculatorBlank/StringCalculator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace StringCalculatorBlank
 {
@@ -6,7 +8,10 @@ namespace StringCalculatorBlank
     {
         public int Add(string numbers)
         {
-            return -42;
+            if (numbers == "") return 0;
+            return numbers.Split(',', 'n')
+                .Select(int.Parse)
+                .Sum();
         }
     }
 }
--- StringCalculatorBlank/StringCalculatorTests.cs
+++ StringCalculatorBlank/StringCalculatorTests.cs
@@ -19,5 +19,65 @@ namespace StringCalculatorBlank
 
             Assert.Equal(0, answer);
         }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        public void OneNum(string numbers, int expected)
+        {
+            var sDigit = new StringCalculator();
+
+            int answer = sDigit.Add(numbers);
+
+            Assert.Equal(expected, answer); 
+        }
+
+        [Theory]
+        [InlineData("1,1", 2)]
+        [InlineData("2,2", 4)]
+        public void TwoNum(string numbers, int expected)
+        {
+            var dDigit = new StringCalculator();
+
+            int answer = dDigit.Add(numbers);
+
+            Assert.Equal(expected, answer);
+        }
+
+        [Theory]
+        [InlineData("1,1,1", 3)]
+        [InlineData("2,2,2,2", 8)]
+        public void MultipleNums(string numbers, int expected)
+        {
+            var dDigit = new StringCalculator();
+
+            int answer = dDigit.Add(numbers);
+
+            Assert.Equal(expected, answer);
+        }
+
+        [Theory]
+        [InlineData("1,\n1,1", 3)]
+        [InlineData("2,2,2,\n2", 8)]
+        public void newLineNums(string numbers, int expected)
+        {
+            var dDigit = new StringCalculator();
+
+            int answer = dDigit.Add(numbers);
+
+            Assert.Equal(expected, answer);
+        }
+
+        [Theory]
+        [InlineData("1,\n1,1", 3)]
+        [InlineData("2,2,2,\n2", 8)]
+        public void delimeterNums(string numbers, int expected)
+        {
+            var dDigit = new StringCalculator();
+
+            int answer = dDigit.Add(numbers);
+
+            Assert.Equal(expected, answer);
+        }
     }
 }
